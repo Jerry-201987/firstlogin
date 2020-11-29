d317810a6f1b724101f0638e5bc2adb18f3de249
Jerry-201987 <54462494+Jerry-201987@users.noreply.github.com>
2020-11-29 13:38:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,112 @@
-123
+// var regPwd = /^(?![a-z]+$)(?![A-Z]+$)(?![(`~!@#$%^&*()-_=+|[{}\];:'",<.>/?)]+$)(?![0-9]+$)[a-zA-Z0-9(`~!@#$%^&*()-_=+|[{}\];:'",<.>/?)]{10,128}$/;
+var regPwd = /^(?![a-zA-Z]+$)(?![A-Z0-9]+$)(?![A-Z\W_!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~ ]+$)(?![a-z0-9]+$)(?![a-z\W_!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~ ]+$)(?![0-9\W_!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~ ]+$)[a-zA-Z0-9\W_!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~ ]{10,32}$/
+var regMoreTwo = /.*(.)\1{2}.*/
+var str = ''
+var localUrl = 'https://10.130.41.73:8447/portal'
+
+//查询
+function getListInfo() {
+  $.ajax({
+    type: "GET",
+    url: localUrl + "/firstlogin/users",
+    success: function (res) {
+      console.log(res);
+      for (let i = 0; i < res.datas.length; i++) {
+        str += '<tr><td class="is-leaf"><div class="cell"><input type="text" userid="' + res.datas[i].userId + '" readonly class="uname" value="' + res.datas[i].username + '"/></div></td>' +
+        '<td class="is-leaf"><div class="cell"><input type="password" placeholder="密码" autocomplete="off" class="pwd_txt" /><div class="errMsg">123</div></div></td>' +
+        '<td class="is-leaf"><div class="cell"><input type="password" placeholder="确认密码" autocomplete="off" class="surePwd_txt" /><div class="errSureMsg"></div></div></td></tr>' 
+      }
+      console.log(str)
+      $("#subject").append(str);
+    },
+    error: function (res) {
+      // let list = {
+      //   "datas": [
+      //     { "userId": "1", "username": "sysadmin" },
+      //     { "userId": "2", "username": "secadmin" },
+      //     { "userId": "3", "username": "secauditor" },
+      //   ],
+      //   "resultCode": "PUB-000000",
+      //   "resultInfo": "success"
+      // }
+      // for (let i = 0; i < list.datas.length; i++) {
+      //   str += '<tr><td class="is-leaf"><div class="cell"><input type="text" userid="' + list.datas[i].userId + '" readonly class="uname" value="' + list.datas[i].username + '"/></div></td>' +
+      //   '<td class="is-leaf"><div class="cell"><input type="password" placeholder="密码" autocomplete="off" class="pwd_txt" /><div class="errMsg">123</div></div></td>' +
+      //   '<td class="is-leaf"><div class="cell"><input type="password" placeholder="确认密码" autocomplete="off" class="surePwd_txt" /><div class="errSureMsg"></div></div></td></tr>' 
+      // }
+      // console.log(str)
+      // $("#subject").append(str);
+    },
+  });
+}
+
+ // 点击确定按钮 修改密码
+function ajaxSure(password, userId) {
+  var arr = []
+  $('.uname').each(function(index,element){
+    arr.push({
+      userId: $(element).attr('userid')
+    })
+  });
+  $('.pwd_txt').each(function(index,element){
+    // console.log(element)
+    arr[index].password = $(element).val()
+  });
+   console.log(arr)
+  $.ajax({
+    type: "PUT",
+    contentType: "application/json;charset=UTF-8",
+    url: localUrl + "/firstlogin/userpasswords",
+    data: JSON.stringify({userpasswords: arr}),
+    success: function (res) {
+      if (res.resultCode === 'PUB-000000') {
+        $('.msg').text('初始化成功').show(300).delay(3000).hide(300)
+        window.location.href = localUrl
+      } else if (res.resultCode === 'FIRST-400000') {
+        $('.msg').text('不再是firstlogin状态').show(300).delay(3000).hide(300)
+      }
+    },
+    error: function (res) {
+      $('.msg').text('初始化失败').show(300).delay(3000).hide(300)
+    },
+  });
+}
+
+$(function () {
+  getListInfo()
+
+  // 重置
+  $(".reset").on("click", function () {
+    $(".pwd_txt").val("");
+    $(".surePwd_txt").val("")
+  });
+
+  // 确定
+  $(".confirm").on("click", function () {
+    var uname = $('.uname').val()
+    var pwd = $(".pwd_txt").val();
+    var surePwd = $(".surePwd_txt").val();
+  //密码为空 密码不符合要求
+  if (!pwd) {
+    $('.msg').text('请输入密码').show(300).delay(3000).hide(300)
+    return false
+  } else if (!(regPwd.test(pwd))) {
+    $('.msg').text('密码不满足要求!').show(300).delay(3000).hide(300)
+    return false
+  } else if (regMoreTwo.test(pwd)) {
+    $('.msg').text('密码不满足要求!').show(300).delay(3000).hide(300)
+    return false
+  } else if (pwd === uname || pwd.split('').reverse().join('') === uname) {
+    $('.msg').text('密码不能与账户一致或者是账户逆序!').show(300).delay(3000).hide(300)
+    return false
+  } else if (!surePwd) {
+    $('.msg').text('请确认密码!').show(300).delay(3000).hide(300)
+    return false
+  } else if (surePwd !== pwd) {
+    $('.msg').text('两次输入密码不一致!').show(300).delay(3000).hide(300)
+    return false
+  } else {
+    ajaxSure();
+  }
+  });
+});
